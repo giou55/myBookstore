bfeb2222462b9a24e43d384fc710854d31eacc32
Giorgos Giourmetakis <giorgosggg@gmail.com>
2019-11-14 16:22:59 +0200

    testing material navbar

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { ModelModule } from "../model/model.module";
@@ -9,11 +10,25 @@ import { CartSummaryComponent } from "./cartSummary.component";
 import { CartDetailComponent } from "./cartDetail.component";
 import { CheckoutComponent } from "./checkout.component";
 import { RouterModule } from "@angular/router";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule, MatListModule, MatMenuModule } from '@angular/material';
 
 @NgModule({
-    imports: [ModelModule, BrowserModule, FormsModule, RouterModule],
+    imports: [ModelModule, CommonModule, BrowserModule, FormsModule, RouterModule, BrowserAnimationsModule, FlexLayoutModule,
+        MatMenuModule,
+        MatListModule,
+        MatButtonModule,
+        MatIconModule,
+        MatToolbarModule,
+        MatSidenavModule],
     declarations: [StoreComponent, CounterDirective, TopbarComponent, CartSummaryComponent,
         CartDetailComponent, CheckoutComponent],
-    exports: [StoreComponent, CartDetailComponent, CheckoutComponent]
+    exports: [StoreComponent, CartDetailComponent, CheckoutComponent,MatMenuModule,
+        MatListModule,
+        MatButtonModule,
+        MatIconModule,
+        MatToolbarModule,
+        MatSidenavModule]
 })
 export class StoreModule { }
